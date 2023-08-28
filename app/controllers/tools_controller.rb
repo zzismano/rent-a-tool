2db05659bdf69692e66aa2c1b937dd8cffd2f082
Alicja Surzyn <alicja.surzyn@gmail.com>
2023-08-28 15:01:55 -0600

    Updated New & Create actions

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -4,5 +4,17 @@ class ToolsController < ApplicationController
   end
 
   def create
+    @tool = Tool.new(tool_params)
+    if @tool.save
+      redirect_to tool_path(@tool), notice: "Tool was successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :price, :description, :availability, :picture )
   end
 end
